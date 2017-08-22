cd0517b608cd17c1c60ac45e681cf4f8f2408117
Sean <sean@johndeMacBook-Pro.local>
2017-08-22 12:01:26 +0800

    WIP: IMPROVING TO ALLOW FULLY CUSTOMIZED SETTINGS

--- js/segmented-slider.js
+++ js/segmented-slider.js
@@ -1,117 +1,120 @@
 (function($) {
-    $.fn.segmentedSlider = function(onChange) {
-        var pointContainer = this.find(".point-container");
-        var leftPadding = +pointContainer.css("padding-left").replace("px", "");
-        var rightPadding = +pointContainer.css("padding-right").replace("px", "");
-        var points = pointContainer.find("li");
-        var greenSegment = this.find(".segment.green");
-        var amberSegment = this.find(".segment.amber");
-        var redSegment = this.find(".segment.red");
-        var greenHandle = this.find(".handle.green");
-        var amberHandle = this.find(".handle.amber");
-        var handleWidth = greenHandle.width();
-        var startPosition = leftPadding;
-        var endPosition = leftPadding + pointContainer.width();
-        var pointInterval = pointContainer.width() / (points.length - 1);
-        var greenPoint = this.data("init-green") || $.fn.segmentedSlider.initGreen;
-        var amberPoint = this.data("init-amber") || $.fn.segmentedSlider.initAmber;
+    $.fn.segmentedSlider = function(options) {
+
+        var sliderContainer = this.find('.slider-container');
+        var handleWidth = options.handleWidth || 20;
+        var handleSpace = handleWidth / 2;
+
+        var segments = [];
+        var segmentDivs = sliderContainer.find('div.segment');
+
+        var colors = options.colors || ['red', 'green', 'orange', 'blue', 'yellow', 'purple'];
 
         (function init() {
-            rearrangePoints();
+            // rearrangePoints();
+            setSegments();
             setHandleDraggable();
 
             // Set initial position of handles
-            setPoint(greenHandle, greenPoint);
-            setPoint(amberHandle, amberPoint);
+            $.each(segments, function(index, segment){
+                setPoint(segment);
+            });
         }());
 
+        function setSegments() {
+            $.each(segmentDivs, function(index, div){
+                // ADD HANDLES TO ALL SEGMENTS EXCEPT LAST
+                if (index!=segmentDivs.length-1) $(div).append('<div class="handle"></div>');
+                // CREATE SEGMENT OBJECT
+                var segment = {
+                    color: colors[index],
+                    element: div,
+                    handle: $(div).find('div.handle') || '',
+                    change: 0
+                };
 
-        function rearrangePoints() {
-            points.each(function(index) {
-                var jObject = $(this);
-                var x = leftPadding + pointInterval * index - jObject.width() / 2;
+                segment.point = parseInt(segment.element.dataset.initPoint) || 100 / segmentDivs.length;
 
-                jObject.css("left", x);
-            })
+                $(segment.element).css({
+                    'background-color': segment.color,
+                    height: options.height
+                });
+                $(segment.handle).css({
+                    width: handleWidth,
+                    height: options.height + 2
+                });
+
+                segments.push(segment);
+            });
         }
 
         function setHandleDraggable() {
-            greenHandle.drag(function(ev, dd) {
-                var x = checkBoundary(dd.offsetX);
-                var tempGreenPoint = convertPositionToPoint(x);
+            // ADD DRAGGABLE OPTION TO ALL HANDLES
+            $.each(segments, function(index, segment){
+                if(segment.handle){
+                    $(segment.handle).drag(function(ev, dd) {
+                        var x = checkBoundary(dd.offsetX);
+                        var tempPoint = convertPositionToPoint(x);
+                        var startPosition = segment.point;
+                        var segSetTotal = segment.point + segments[index+1].point;
 
-                greenPoint = (tempGreenPoint <= amberPoint) ? tempGreenPoint : amberPoint;
-                setPoint(greenHandle, greenPoint);
-            }, {
-                relative: true
-            });
-            amberHandle.drag(function(ev, dd) {
-                var x = checkBoundary(dd.offsetX);
-                var tempAmberPoint = convertPositionToPoint(x);
+                        segment.point = tempPoint <= 1 ? 1 : tempPoint>=segSetTotal-1 ? segSetTotal-1 : tempPoint;
 
-                amberPoint = (tempAmberPoint >= greenPoint) ? tempAmberPoint : greenPoint;
-                setPoint(amberHandle, amberPoint);
-            }, {
-                relative: true
+                        console.log({ segment: segment.point, nextSeg: segments[index+1].point, total: segSetTotal, change: segment.change });
+
+                        // segment.change = segment.point - startPosition;
+                        segments[index+1].point = segSetTotal - segment.point;
+
+                        setPoint(segment);
+                    }, {
+                        relative: true
+                    }).drag( 'dragend', function(){
+                        if (options.onDrop && typeof options.onDrop === "function") {
+                            options.onDrop();
+                        }
+                    }); 
+                }
             });
         }
 
         // Helpers
         function checkBoundary(x) {
-            if (x + handleWidth / 2 < startPosition) {
-                return startPosition - handleWidth / 2;
-            } else if (x + handleWidth / 2 > endPosition) {
-                return endPosition - handleWidth / 2;
+            if (x + handleSpace <= handleSpace) {
+                return 0;
+            } else if (x + handleSpace > sliderContainer.width()) {
+                return sliderContainer.width() - handleSpace;
             }
 
             return x;
         }
 
-        function convertPointToPosition(point, center) {
-            var position = startPosition;
-
-            position += (point) / 10 * pointInterval;
-            if (center === undefined || !center) {
-                position -= handleWidth / 2;
-            }
-
-            return position;
+        function convertPointToPosition(point) {
+            return ((point) / 100) * sliderContainer.width();
         }
 
         function convertPositionToPoint(position) {
-            return (position + handleWidth / 2 - startPosition) / pointInterval * 10
+            return position / sliderContainer.width() * 100;
         }
 
-        function setPoint(handle, point) {
-            handle.css({
-                left: convertPointToPosition(point)
+        function setPoint(segment) {
+            var index = $(segments).index(segment);
+            segment.position = convertPointToPosition(segment.point);
+            // THIS SEG
+            $(segment.element).css({
+                width: segment.point + '%'
             });
-            updateSegments();
-            if (onChange && typeof onChange === "function") {
-                onChange(greenPoint, amberPoint);
+            $(segment.handle).css({
+                right: -(handleSpace)
+            });
+
+            if ( index!=segments.length-1 ) {
+                // NEXT SEG
+                $(segments[index+1].element).css({
+                    left: convertPositionToPoint(segment.position + $(segment.element)[0].offsetLeft) + '%',
+                    width: segments[index+1].point + '%'
+                });
             }
-        }
-
-        function updateSegments() {
-            var greenPosition = convertPointToPosition(greenPoint, true);
-            var amberPosition = convertPointToPosition(amberPoint, true);
-
-            greenSegment.css({
-                width: greenPosition
-            });
-            amberSegment.css({
-                left: greenPosition,
-                width: amberPosition - greenPosition
-            });
-            redSegment.css({
-                left: amberPosition,
-                width: endPosition + leftPadding - amberPosition
-            });
         }
     }
 
-    $.fn.segmentedSlider.defaults = {
-        initGreen: 10,
-        initAmber: 50
-    };
 }(jQuery));
